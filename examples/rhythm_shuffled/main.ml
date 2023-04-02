e05a1551e6b298f6bdd2dcdc2e788ce0ab80b9ae
rand00 <oth.rand@gmail.com>
2023-04-02 13:17:31 +0200

    examples/rhythm_shuffled: Added example where rhythm is shuffled on each initial beat

---
+++ examples/rhythm_shuffled/main.ml
@@ -0,0 +1,34 @@
+open Lwt_react
+open Lwt.Syntax
+
+let bpm = 120. 
+
+module Beat = Fry.Beat.Make(struct
+    let bpm_s = S.const bpm
+  end)
+
+let rhythm = 
+  [ false; false; false; true; true; true ] 
+
+let rhythm_e =
+  Beat.e
+  |> E.fold (fun (_, rhythm') tick ->
+    if Fry.Rhythm.index ~tick rhythm = 0 then
+      let rhythm' = Fry.Rhythm.shuffle rhythm in
+      tick, rhythm'
+    else
+      tick, rhythm'
+  ) (-1, rhythm)
+  |> E.fmap (fun (tick, rhythm) -> 
+    if Fry.Rhythm.Bool.is_on ~tick rhythm then
+      Some tick
+    else
+      None
+  )
+
+let _out =
+  rhythm_e |> E.trace (fun i ->
+    Printf.printf "probabilistic notes in a rhythm = %d\n%!" i
+  )
+
+let () = Lwt_main.run @@ Beat.run ()
