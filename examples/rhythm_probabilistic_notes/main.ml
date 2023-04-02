49b8f4ef199a56d7b1ec02095379243506f9cd7c
rand00 <oth.rand@gmail.com>
2023-04-02 12:50:32 +0200

    examples/rhythm_probabilistic_notes: Added example for probabilistic notes in a rhythm

---
+++ examples/rhythm_probabilistic_notes/main.ml
@@ -0,0 +1,35 @@
+open Lwt_react
+open Lwt.Syntax
+
+let bpm = 120. 
+
+module Beat = Fry.Beat.Make(struct
+    let bpm_s = S.const bpm
+  end)
+
+let rhythm = Fry.Rhythm.(
+  [ true; true; true; true ] |> Bool.mapi (function
+    | 0 -> `Prob 90.
+    | 1 -> `Prob 100.
+    | 2 -> `Prob 20.
+    | 3 -> `Prob 100.
+    | _ -> `Prob 100.
+  )
+)
+
+let rhythm_e =
+  Beat.e
+  |> E.fmap (fun tick -> CCOption.Infix.(
+    Fry.Rhythm.Option.get ~tick rhythm >>= fun (`Prob p) ->
+    if Random.float 100. < p then
+      Some tick
+    else
+      None
+  ))
+
+let _out =
+  rhythm_e |> E.trace (fun i ->
+    Printf.printf "euclidian rhythm with delayed 3rd note (out of 4) = %d\n%!" i
+  )
+
+let () = Lwt_main.run @@ Beat.run ()
