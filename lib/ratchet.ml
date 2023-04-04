8ef98675e1e73bc087018b2958d9325b06cf5f89
rand00 <oth.rand@gmail.com>
2023-04-04 21:49:24 +0200

    lib/Ratchet: Simplified 'choose' a lot (almost no logic left..), which has the same power

--- lib/ratchet.ml
+++ lib/ratchet.ml
@@ -6,22 +6,8 @@ let every ~n ~switch_e e =
   |> E.map (fun i -> if i mod n = 0 then switch_e else e)
   |> E.switch e
 
-let choose ~on e =
-  e 
-  |> E.fold (fun (i, _) e_v -> succ i, Some e_v) (-1, None)
-  |> E.fmap (fun (i, opt_e_v) -> match opt_e_v with
-    | None -> None
-    | Some e_v -> Some (i, e_v)
-  )
-  |> E.map (fun (i, v) ->
-    match on ~v ~i with
-    | Some (ratchet_e, ratchet_f) ->
-      ratchet_e |> E.fold (fun (i_ratchet, _) v_ratchet ->
-        let v = ratchet_f ~v_ratchet ~i_ratchet in
-        succ i_ratchet, v
-      ) (0, None)
-      |> E.fmap (fun (_, opt_e_v) -> opt_e_v)
-    | None -> e
-  )
+let choose chooser e =
+  e
+  |> E.map CCFun.(chooser%>CCOption.get_or ~default:e)
   |> E.switch e
 
