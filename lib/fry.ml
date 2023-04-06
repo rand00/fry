86a0763b4645cf70cb89d0e70ba4af66ba35c785
rand00 <oth.rand@gmail.com>
2023-04-06 15:14:36 +0200

    lib/Fry: Forgot to add Tuple module

--- lib/fry.ml
+++ lib/fry.ml
@@ -2,6 +2,7 @@
 (*goto just make dune package these? won't there come operators in here?*)
 
 module Eq = Eq
+module Tuple = Tuple
 module Beat = Beat
 module Ratchet = Ratchet
 module Rhythm = Rhythm
@@ -9,7 +10,6 @@ module Time = Time
 module Signal = Signal
 module Event = Event
 module Envelope = Envelope
-
 
 
 
