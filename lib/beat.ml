06aad3ad830dddadc3adda8473fbfce9307f184f
rand00 <oth.rand@gmail.com>
2023-06-22 01:30:50 +0200

    lib/Beat: Adjusting timings of internal tick and derived events - now bpm is correct

--- lib/beat.ml
+++ lib/beat.ml
@@ -1,39 +1,51 @@
 open Lwt_react 
 open Lwt.Infix
 
+(*> goto don't expose fps_supd*)
 let fps_s, fps_supd = S.create 333.
     
+(*> goto don't expose tick_eupd*)
 let (tick_e : int E.t), tick_eupd = E.create ()
 
 let make ~bpm_s = 
   let eps = 0.000001 in
-  let shoot (_out, out_tick, ticks_since_beat) (tick, (fps, bpm)) =
+  let shoot (_out, out_tick, ticks_since_beat, diff_time) (tick, (fps, bpm)) =
     if bpm < eps then
-      None, out_tick, ticks_since_beat
+      None, out_tick, ticks_since_beat, 0.
     else 
       let time_bpm = Time.of_bpm bpm in
-      let time_since_start = float ticks_since_beat *. (1. /. fps) in
-      if time_since_start >= time_bpm then
+      let ticks_since_beat = succ ticks_since_beat in
+      let time_since_beat = float ticks_since_beat *. (1. /. fps) in
+      let time_since_beat_adjusted = time_since_beat +. diff_time in
+      if time_since_beat_adjusted >= time_bpm then
         let out = Some out_tick in
         let out_tick = succ out_tick in
         let ticks_since_beat = 0 in
-        out, out_tick, ticks_since_beat
+        let diff_time = time_since_beat_adjusted -. time_bpm in
+        out, out_tick, ticks_since_beat, diff_time
       else
         let out = None in
-        let ticks_since_beat = succ ticks_since_beat in
-        out, out_tick, ticks_since_beat
+        out, out_tick, ticks_since_beat, diff_time
   in
-  S.l2 Tuple.mk2 fps_s bpm_s
+  S.l2 ~eq:Eq.never Tuple.mk2 fps_s bpm_s
   |> S.sample Tuple.mk2 tick_e 
-  |> E.fold shoot (None, 0, 0)
-  |> E.fmap (fun (out, _, _) -> out)
+  |> E.fold shoot (None, 0, 0, 0.)
+  |> E.fmap (fun (out, _, _, _) -> out)
 
-let run ~sleep ~max_bpm () =
+let run ~sleep ~time ~max_bpm () =
   let fps = Time.fps_of_bpm max_bpm in
+  fps_supd fps;
+  let perfect_sleep_time = 1. /. fps in
+  let start = time () in
   let rec loop tick =
     tick_eupd tick;
-    fps_supd fps;
-    sleep (1. /. fps) >>= fun () -> 
+    let perfect_time = start +. float tick *. Time.of_bpm max_bpm in
+    let now = time () in
+    let diff_time = perfect_time -. now in
+    let sleep_time =
+      CCFloat.max 0. (perfect_sleep_time +. diff_time)
+    in
+    sleep sleep_time >>= fun () -> 
     loop @@ succ tick
   in
   loop 0
